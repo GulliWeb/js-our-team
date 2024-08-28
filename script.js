67f42891626ee80888818f173ef65009c635808e
Andrea Gulli <andreagulli11@gmail.com>
2024-08-28 16:15:27 +0200

    completed MILESTONE 1

---
+++ script.js
@@ -0,0 +1,35 @@
+// Creo un array di oggetti
+const teamMembers = [ 
+    {
+        'name': 'Wayne Barnett',
+        'role': 'Founder & CEO',
+        'images': 'wayne-barnett-founder-ceo.jpg'
+      },
+      {
+        'name': 'Angela Caroll',
+        'role': 'Chief Editor',
+        'images': 'angela-caroll-chief-editor.jpg'
+      },
+      {
+        'name': 'Walter Gordon',
+        'role': 'Office Manager',
+        'images': 'walter-gordon-office-manager.jpg'
+      },
+      {
+        'name': 'Angela Lopez',
+        'role': 'Social Media Manager',
+        'images': 'angela-lopez-social-media-manager.jpg'
+      },
+      {
+        'name': 'Scott Estrada',
+        'role': 'Developer',
+        'images': 'scott-estrada-developer.jpg'
+      },
+      {
+        'name': 'Barbara Ramos',
+        'role': 'Graphic Designer',
+        'images': 'barbara-ramos-graphic-designer.jpg'
+      }
+    ];
+
+
